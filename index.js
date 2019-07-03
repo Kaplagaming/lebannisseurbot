8b80fd5017e890cc9f0ec76300ae58c99281189b
Kaplagaming <50170486+Kaplagaming@users.noreply.github.com>
2019-07-03 10:59:34 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg = require('./index.json');
+const prefix = ("/");
+
+bot.on('ready' , function() {
+    console.log("je suis prêt à ètre utiliser.")
+});
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('bienvenue sur le serveur test bot .' + member.displayName)
+    })
+});
+
+
+bot.login(cfg.token)
